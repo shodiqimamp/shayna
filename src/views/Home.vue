e6aec582eb0b6e435142917e3a2930020867f9c6
shodiqimamp <shodiqimamp@gmail.com>
2022-04-07 22:03:47 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna/>
+    <HeroSection/>
+    <BannerShayna/>
+    <InstaShayna/>
+    <PartnerShayna/>
+    <FooterShayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import BannerShayna from '@/components/BannerShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+export default 
+{
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroSection,
+    BannerShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+    
+  }
+}
+</script>
